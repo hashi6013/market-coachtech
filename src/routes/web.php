703a333d2f376625bc1c1f93fee854fe849c66e9
hashimoto takumi <takumih6013@gmail.com>
2024-06-26 07:14:21 +0900

    ルーティングの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\AddressEditController;
+use App\Http\Controllers\MypageController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +20,15 @@ use App\Http\Controllers\ItemController;
 */
 
 Route::get('/', [ItemController::class, 'index']);
+Route::get('/search', [ItemController::class, 'search']);
+Route::get('/item/favorite/{id}', [FavoriteController::class, 'favorite']);
+Route::get('/item/unlike/{id}', [FavoriteController::class, 'unlike']);
+Route::get('/item/{item_id}', [ItemController::class, 'itemDetail']);
+Route::get('/purchase/{item_id}', [PurchaseController::class, 'purchase']);
+Route::get('/sell', [ItemController::class, 'sell']);
 
-
+// 仮
+Route::get('/purchase/address/{item_id}', [AddressEditController::class, 'addressEdit']);
+Route::get('/mypage', [MypageController::class, 'mypageIndex']);
+Route::get('/mypage/profile', [MypageController::class, 'update']);
+Route::get('/comment', [CommentController::class, 'comment']);
